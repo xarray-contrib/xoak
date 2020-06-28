add3896349256b28983c98e77a94e8e723078615
Willi Rath <willirath@users.noreply.github.com>
2020-06-28 08:28:17 +0200

    Set master doc for RTD

--- doc/conf.py
+++ doc/conf.py
@@ -25,6 +25,8 @@ author = "Benoît Bovy, Willi Rath"
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
