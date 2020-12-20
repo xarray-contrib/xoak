2cc512daa52a3df4f95535a3ead0b4461b1ab5aa
Willi Rath <wrath@geomar.de>
2020-12-20 16:11:59 +0100

    Add two-step test via indexers

--- src/xoak/tests/test_scipy_adapters.py
+++ src/xoak/tests/test_scipy_adapters.py
@@ -13,6 +13,15 @@ def test_scipy_kdtree(xyz_dataset, xyz_indexer, xyz_expected):
     xr.testing.assert_equal(ds_sel.load(), xyz_expected.load())
 
 
+def test_scipy_kdtree_via_indexer(xyz_dataset, xyz_indexer, xyz_expected):
+    xyz_dataset.xoak.set_index(['x', 'y', 'z'], 'scipy_kdtree')
+
+    indexers = xyz_dataset.xoak.get_indexers(x=xyz_indexer.xx, y=xyz_indexer.yy, z=xyz_indexer.zz)
+    ds_sel = xyz_dataset.isel(indexers)
+
+    xr.testing.assert_equal(ds_sel.load(), xyz_expected.load())
+
+
 def test_scipy_kdtree_options():
     ds = xr.Dataset(coords={'x': ('points', [1, 2]), 'y': ('points', [1, 2])})
 
