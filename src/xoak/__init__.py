30b7175f24d4cbb83d24eb6e3bf2813daf7a80c7
rafa-guedes <ocerafa@gmail.com>
2025-10-01 13:36:25 +1300

    replace deprecated pkg_resources package

--- src/xoak/__init__.py
+++ src/xoak/__init__.py
@@ -1,10 +1,14 @@
-from pkg_resources import DistributionNotFound, get_distribution
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    # Python < 3.8
+    from importlib_metadata import version, PackageNotFoundError
 
 from .accessor import XoakAccessor
 from .index import IndexAdapter, IndexRegistry
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:  # pragma: no cover
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
     # package is not installed
     pass
