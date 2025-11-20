0760382bad14d5cb92b4151eeb3221466a30bb8f
Benoit Bovy <benbovy@gmail.com>
2025-11-20 11:15:34 +0100

    switch to pyproject / hatchling

--- src/xoak/__init__.py
+++ src/xoak/__init__.py
@@ -1,8 +1,4 @@
-try:
-    from importlib.metadata import version, PackageNotFoundError
-except ImportError:
-    # Python < 3.8
-    from importlib_metadata import version, PackageNotFoundError
+from importlib.metadata import version
 
 from .accessor import XoakAccessor
 from .index import IndexAdapter, IndexRegistry
@@ -13,8 +9,4 @@ __all__ = [
     "IndexRegistry",
 ]
 
-try:
-    __version__ = version(__name__)
-except PackageNotFoundError:  # pragma: no cover
-    # package is not installed
-    pass
+__version__ = version("xoak")
